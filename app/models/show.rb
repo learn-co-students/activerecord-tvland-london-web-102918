8cf3b12ae725cd99bcbdf4905b58d92f9b19a69c
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-11 21:32:17 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(call_letters:)
+    network = Network.create(call_letters: call_letters)
+    self.network = network
+  end
+
 end
